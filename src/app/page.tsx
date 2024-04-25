a485f233fc9a7dd15422da8495dd6e0d7cf654e7
Kabir <kabirshuvo19@gmail.com>
2024-04-25 16:34:35 +0600

    Darkmode Activated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/modeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <nav>
+        <ModeToggle />
+      </nav>
       <h1>Sapakhana With shadcn</h1>
+
       <Button variant={"default"}>ShadCN Button</Button>
     </main>
   );
