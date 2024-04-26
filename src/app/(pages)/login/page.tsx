6e63f93d0e9e2774a20e0284d57918f362e35aed
Kabir <kabirshuvo19@gmail.com>
2024-04-26 15:45:29 +0600

    api folder created and login signup page added

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LogInPage = () => {
+  return (
+    <div>
+      <h1> const LogInPage = export default LogInPage</h1>
+    </div>
+  );
+};
+
+export default LogInPage;
