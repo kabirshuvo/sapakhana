50b0fe7842376fbf6cc53de84919d37965a076cf
Kabir <kabirshuvo19@gmail.com>
2024-05-01 18:57:16 +0600

    getData from token exported from helpers

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,3 +1,4 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
 import { NextRequest, NextResponse } from "next/server";
 import { connect } from "@/dbConfig/dbConfig";
 import User, { UserDocument } from "@/models/userModel";
@@ -7,3 +8,18 @@ import jwt from "jsonwebtoken";
 connect();
 
 export async function POST(request: NextRequest) {}
+
+connect();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({ _id: userId }).select("-password");
+    return NextResponse.json({
+      mesaaage: "User found",
+      data: user,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
